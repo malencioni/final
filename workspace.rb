b6cb341335e9642c837f1fed1de6384753e80d25
malencioni <michael.lencioni@kellogg.northwestern.edu>
2020-08-13 15:59:55 +0000

    Update

--- workspace.rb
+++ workspace.rb
@@ -86,3 +86,39 @@ end
                         <% @thisthatandanother = @winerywines_table2.where([:wid] => params["wid"])%>
                         <% @thisthatandanother1 = @thisthatandanother#{:wid}
                         <% @thatotherthing =  @thisthatandanother.where([:wtid] => @winery_winecategory2[:wtid])%>
+
+
+
+ps_endpoint = ENV['EINSTEIN_VISION_URL']
+subject = ENV['EINSTEIN_VISION_ACCOUNT_ID']
+private_key = String.new(ENV['EINSTEIN_VISION_PRIVATE_KEY'])
+private_key.gsub!('\n', "\n")
+expiry = Time.now.to_i + (60 * 15)
+
+# Read the private key string as Ruby RSA private key
+rsa_private = OpenSSL::PKey::RSA.new(private_key)
+
+# Build the JWT payload
+payload = {
+        :sub => subject,
+        :aud => "https://api.einstein.ai/v2/oauth2/token",
+        :exp => expiry
+    }
+
+# Sign the JWT payload
+assertion = JWT.encode payload, rsa_private, 'RS256'
+puts assertion
+
+# Call the OAuth endpoint to generate a token
+response = RestClient.post(ps_endpoint + 'v2/oauth2/token', {
+        grant_type: 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+        assertion: assertion
+    })
+
+token_json = JSON.parse(response)
+puts "\nGenerated access token:\n"
+puts JSON.pretty_generate(token_json)
+
+access_token = token_json["access_token"]
+
+#######################################################################################
